b2cd6f82b9899316f1623c45770983d236d5af59
Matt <matt.hong@adriel.com>
2022-07-11 21:58:49 +0900

    updated db info

--- coronaboard-api/database/index.js
+++ coronaboard-api/database/index.js
@@ -4,8 +4,8 @@ const config = {
   host: process.env.CORONABOARD_MYSQL_HOST || "127.0.0.1",
   port: 3306,
   database: "coronaboard",
-  user: "coronaboard_admin",
-  password: process.env.CORONABOARD_MYSQL_PASSWORD || "1234",
+  user: "admin",
+  password: process.env.CORONABOARD_MYSQL_PASSWORD || "coronaboard",
 };
 
 const sequelize = new Sequelize(config.database, config.user, config.password, {
