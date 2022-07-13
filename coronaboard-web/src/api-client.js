d2176612ec45c8280e1fdca0799c92a3ffefe974
Matt <matt.hong@adriel.com>
2022-07-13 13:28:54 +0900

    updated ip address

--- coronaboard-web/src/api-client.js
+++ coronaboard-web/src/api-client.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 class ApiClient {
   constructor() {
     const client = axios.create({
-      baseURL: process.env.CB_API_BASE_URL || "http://15.164.102.242",
+      baseURL: process.env.CB_API_BASE_URL || "http://localhost:8080",
     });
 
     client.interceptors.response.use((resp) => {
